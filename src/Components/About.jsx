e82697829ae3243e58d97be07201418736a2a54c
Prajaktaapte91 <itsprajaktaapte@gmail.com>
2024-10-07 16:39:37 +1300

    text Alignment

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -41,10 +41,10 @@ function About() {
                 loop={true}
               />
             </div>
-            <p className="mt-4 text-sm text-white md:text-md ">
-            <p>I have solid foundation in front-end technologies like HTML5, CSS3, JavaScript, and frameworks such as React and Next JS. I have extensive experience creating responsive, user-friendly, and visually appealing web interfaces using CSS Frameworks like Material UI and Tailwind CSS. My expertise also includes integrating third-party APIs, and deploying web applications on Azure or AWS</p>
+            <p className="text-justify mt-4 text-sm text-white md:text-md ">
+            <p className="text-justify">I have solid foundation in front-end technologies like HTML5, CSS3, JavaScript, and frameworks such as React and Next JS. I have extensive experience creating responsive, user-friendly, and visually appealing web interfaces using CSS Frameworks like Material UI and Tailwind CSS. My expertise also includes integrating third-party APIs, and deploying web applications on Azure or AWS.</p>
             <br />
-<p>I focus on maintaining code quality, and adhering to best practices, ensuring that your digital presence stands out and performs exceptionally. Whether you need a sleek website, a robust web application, or tailored UI components, I am here to bring your ideas to life and provide top-notch front-end solutions that align with your business goals.</p>           </p>
+<p className="text-justify">I focus on maintaining code quality, and adhering to best practices, ensuring that your digital presence stands out and performs exceptionally. Whether you need a sleek website, a robust web application, or tailored UI components, I am here to bring your ideas to life and provide top-notch front-end solutions that align with your business goals.</p>           </p>
             
             {/* social media icons  */}
             <div className=" flex justify-between">
